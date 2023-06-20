fa9945b035cbee9e08e8186fd4dcf250cc4c5ec7
Yudai Nakamura <c2p31080@bunkyo.ac.jp>
2023-06-20 21:19:33 +0900

    main

---
+++ v_frequest_list.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <?php include("./html/meta.htm"); ?>
+    <title>拾得物管理システム</title>
+</head>
+
+<body>
+    <?php include("./html/header.htm"); ?>
+    <div class="container">
+        <div class="row mt-3 mb-1">
+            <div class="col">
+                <p class="h2 p-1  bg-primary w-50 rounded text-center text-white">捜索依頼一覧</p>
+            </div>
+        </div>
+        <?php include("./html/footer.htm"); ?>
+        <script src="./js/main.js"></script>
+</body>
+
+</html>
