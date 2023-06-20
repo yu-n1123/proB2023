2712da706e80eff2b5bae7e4d55b03bf68e6d3fe
Yudai Nakamura <c2p31080@bunkyo.ac.jp>
2023-06-20 23:08:50 +0900

    main

--- js/main.js
+++ js/main.js
@@ -102,3 +102,28 @@ function check_user(){
   });
   return f;
 }
+
+function get_frequest(tag_id, key_id){
+  $.getJSON(API_URL, {type:"json", data:"frequest_items_list"}, function(j){
+    var src = "<table class='table table-primary table-striped'><tr><th>大分類</th><th>物品名</th><th>拾得場所</th><th>色</th><th>特徴</th><th>日時</th><th>状態</th><th>操作</th></tr>";
+    cnt=0;
+    key=$("#"+key_id).val();
+    $.each(j, function(i,v){
+      if(v["物品名"].indexOf(key)>-1){
+        src += "<tr>";
+        src += "<td>" + v["大分類"] + "</td>";
+        src += "<td>" + v["物品名"] + "</td>";
+        src += "<td>" + v["拾得場所"] + "</td>";
+        src += "<td>" + v["色"] + "</td>";
+        src += "<td>" + v["特徴"] + "</td>";
+        src += "<td>" + v["変更日時"] + "</td>";
+        src += "<td>" + v["変更内容"] + "</td>";
+        src += "</tr>";
+        cnt++;
+      }
+    });
+    src += "</table>";
+    src = "検索結果：" + cnt + "件" + src;
+    $("#"+tag_id).html(src);
+  }).fail(function(e){ console.log(e); });
+}
