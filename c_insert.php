ceadd80681b40c21bf9f4bddec77663f8501966e
Yudai Nakamura <c2p31080@bunkyo.ac.jp>
2023-06-20 21:55:20 +0900

    main

--- c_insert.php
+++ c_insert.php
@@ -49,6 +49,7 @@ if($_POST["data"] == "fitem"){
     $arr = array($_POST["p1"],$_POST["p2"],$_POST["p3"],$_POST["p4"],$_POST["p5"]);
     $stmt = $my->prepare($sql);
     $stmt->execute($arr);
+    $np = "v_frequest.php";
   }
 }
 header("location:{$np}");
