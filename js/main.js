90aa7d4924e03d4dcf50f05f755f2c3d45fb80e2
Yudai Nakamura <c2p31080@bunkyo.ac.jp>
2023-06-20 19:32:42 +0900

    main

---
+++ js/main.js
@@ -0,0 +1,104 @@
+﻿nl=[];
+var API_URL = "http://localhost/probc/c_json.php";
+
+function get_fitem(tag_id, key_id){
+  $.getJSON(API_URL, {type:"json", data:"find_item"}, function(j){
+    var src = "<table class='table table-success table-striped'><tr><th>大分類</th><th>物品名</th><th>拾得場所</th><th>色</th><th>特徴</th><th>日時</th><th>状態</th><th>操作</th></tr>";
+    cnt=0;
+    key=$("#"+key_id).val();
+    $.each(j, function(i,v){
+      if(v["物品名"].indexOf(key)>-1){
+        src += "<tr>";
+        src += "<td>" + v["大分類"] + "</td>";
+        src += "<td>" + v["物品名"] + "</td>";
+        src += "<td>" + v["拾得場所"] + "</td>";
+        src += "<td>" + v["色"] + "</td>";
+        src += "<td>" + v["特徴"] + "</td>";
+        src += "<td>" + v["変更日時"] + "</td>";
+        src += "<td>" + v["変更内容"] + "</td>";
+        src += "<td><a href='v_fitem_detail.php?id=" + v["拾得物ID"] + "' class='btn btn-success'>詳細</a></td>";
+        src += "</tr>";
+        cnt++;
+      }
+    });
+    src += "</table>";
+    src = "検索結果：" + cnt + "件" + src;
+    $("#"+tag_id).html(src);
+  }).fail(function(e){ console.log(e); });
+}
+
+function get_fitem_detail(tag_ids, fid){
+  $.getJSON(API_URL, {type:"json", data:"find_item_detail", fid:fid}, function(j){
+    var src = "<table class='table table-success'>";
+    cnt=0;
+    v = j["拾得物"];
+    src += "<tr><th>大分類</th><td>" + v["大分類"] + "</td></tr>";
+    src += "<tr><th>物品名</th><td>" + v["物品名"] + "</td></tr>";
+    src += "<tr><th>拾得場所</th><td>" + v["拾得場所"] + "</td></tr>";
+    src += "<tr><th>色</th><td>" + v["色"] + "</td></tr>";
+    src += "<tr><th>特徴</th><td>" + v["特徴"] + "</td></tr>";
+    src += "</table>";
+    var src2 = "<table class='table table-success'><tr><th>日時</th><th>状態</th><th>対応者</th></tr>";
+    $.each(j["変更履歴"], function(i,v){
+      src2 += "<tr>";
+      src2 += "<td>" + v["変更日時"] + "</td>";
+      src2 += "<td>" + v["変更内容"] + "</td>";
+      src2 += "<td>" + v["氏名"] + "</td>";
+      src2 += "</tr>";
+    });
+    src2 += "</table>";
+    $("#"+tag_ids[0]).html(src);
+    $("#"+tag_ids[1]).html(src2);
+  }).fail(function(e){ console.log(e); });
+}
+
+$(function(){
+  $(".needs-validation").each(function(i,f){
+    f.addEventListener("submit", function(e){
+      if(!check_user() || !f.checkValidity()){
+        e.preventDefault();
+        e.stopPropagation();
+      }
+      f.classList.add("was-validated");
+    }, false);
+  });
+  $(".user").on("blur", check_user);
+  $("select.belongs").each(function(i,s){
+    $.getJSON(API_URL, {type:"json", data:"belongs"}, function(j){
+      src = "";
+      $.each(j, function(i,v){
+        src += "<option value='" + v["所属ID"] + "'>" + v["所属名"] + " / " + v["所属分類"] + "</option>";
+      });
+      $(s).html(src);
+    });
+  });
+  $("select.category").each(function(i,s){
+    $.getJSON(API_URL, {type:"json", data:"category"}, function(j){
+      src = "";
+      $.each(j, function(i,v){
+        src += "<option value='" + v["拾得物分類ID"] + "'>" + v["大分類"] + " / " + v["物品名"] + "</option>";
+      });
+      $(s).html(src);
+    });
+  });
+  $.getJSON(API_URL, {type:"json", data:"user"}, function(j){
+    $.each(j, function(i,v){
+      nl[v["ユーザID"]]=v["氏名"];
+    });
+  });
+});
+
+
+function check_user(){
+  f = true
+  $(".user").each(function(i,tag){
+    if(nl.includes($(tag).val())){
+      $(tag).next(".valid-feedback").html("登録済みのユーザです");
+      $(tag).nextAll(".userid").val(nl.indexOf($(tag).val()));
+    }else{
+      $(tag).next(".valid-feedback").html("<span class='text-danger'>そのユーザは存在しません</span>");
+      f = false;
+    }
+  });
+  return f;
+}
---
+++ v_fitem_search.php
@@ -0,0 +1,33 @@
+﻿<!doctype html>
+<html lang="ja">
+<head>
+<?php include("./html/meta.htm"); ?>
+  <title>拾得物管理システム</title>
+</head>
+
+<body>
+  <?php include("./html/header.htm"); ?>
+  
+  <div class="container">
+    <div class="row">
+      <div class="col mt-3">
+        <p class="h2 p-1  bg-success w-50 rounded text-center text-white">拾得物検索</p>
+        <p class="bg-light p-1 border m-0">拾得物検索機能では，文教大学湘南キャンパスの拾得物について，届けられているものを検索する機能です．検索キーワードから物品名を入力して検索してください．検索結果の件数と一覧が表形式で一覧表示されます．詳細を確認するには，詳細を選択してください．</p>
+        <hr>
+        物品名
+        <input type="text" name="key" id="key">
+        <button class="btn btn-success" onclick="get_fitem('r','key')">検索</button>
+      </div>
+    </div>
+
+    <div class="row mt-3">
+      <div class="col">
+        <span id="r"></span>
+      </div>
+    </div>
+
+  </div>
+  <?php include("./html/footer.htm"); ?>
+  <script src="./js/main.js"></script>
+</body>
+</html>
